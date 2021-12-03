76ef2af8f550476b277fc63fb9bd4b19d7c14e2f
Pablo Reyes Robles <pabloreyes500@gmail.com>
2021-12-02 23:54:01 -0300

    processing core need testing

--- tarea_2/coprocessor/coprocessor.srcs/sources_1/new/processing_core.sv
+++ tarea_2/coprocessor/coprocessor.srcs/sources_1/new/processing_core.sv
@@ -4,15 +4,21 @@ module processing_core #(
 (
   input   logic clk,
   input   logic reset,
+  input   logic rx_ready,
+  input   logic tx_busy,
   input   logic cmd_flag,
   input   logic [CMD_WIDTH - 1:0] cmd_dec,
   input   logic bram_sel,
   input   logic [7:0] brama_read,
-  output  logic brama_write_addr,
+  input   logic [7:0] bramb_read,
+  output  logic [9:0] brama_write_addr,
+  output  logic [9:0] brama_read_addr,
   output  logic brama_write_en,
-  output  logic brama_read_addr,
+  output  logic [9:0] bramb_write_addr,
+  output  logic [9:0] bramb_read_addr,
+  output  logic bramb_write_en,
   output  logic core_lock,
-  output  logic tx_data
+  output  logic [7:0] tx_data
 );
   
   typedef enum logic [3:0] {IDLE, WRITE_BRAM, READ_VEC, SUM_VEC, AVG_VEC, MAN_DIST, EUC_DIST, TO_HOST} state;
@@ -23,61 +29,258 @@ module processing_core #(
   localparam AVG_CMD    = 3'd4;
   localparam MAN_CMD    = 3'd5;
   localparam EUC_CMD    = 3'd6;
+
+  localparam MEMORY_DEPTH = 1024; // 8 For testing
+  localparam ADDRESS_WIDTH = $clog2(MEMORY_DEPTH);
+  localparam WAIT_READ_CYCLES = 3;
   
   state current_state, next_state;
+  logic write_done, enable_write, write_enable, tx_start, tx_busy;
+  logic brama_read_done, brama_byte_read, brama_fetch, brama_queued;
+  logic bramb_read_done, bramb_byte_read, bramb_fetch, bramb_queued;
+
+  logic [19:0] dist_accum;
+  logic [15:0] diff;
+
+  logic man_accum_flag, man_accum_done;
+  logic euc_accum_flag, euc_accum_done;
 
   always_ff @(posedge clk) begin
     if (~reset) begin
       current_state <= IDLE;
     end
     else current_state <= next_state;
+
+    if (man_accum_flag) begin
+      dist_accum <= diff[15] == 1 ? dist_accum - diff : dist_accum + diff; // CHECK TESTBENCH
+    end
+
+    if (euc_accum_flag) begin
+      dist_accum <= dist_accum + (diff * diff); // CHECK TESTBENCH
+    end
   end
 
   always_comb begin
+    core_lock = 1'b0;
+    man_accum_flag = 1'b0;
+    euc_accum_flag = 1'b0;
+    brama_fetch = 1'b0;
+    bramb_fetch = 1'b0;
+
+    brama_write_en = 1'b0;
+    bramb_write_en = 1'b0;
+
+    // COMPLETE DEFAULT VALUES
+    
     case (current_state)
+
       IDLE: begin
         if (cmd_flag) begin
-          core_lock = 1'b1;
           case (cmd_dec)
             WRITE_CMD:  next_state = WRITE_BRAM;
             READ_CMD:   next_state = READ_VEC;
-            SUM_CMD:    next_state = SUM_VEC;
-            AVG_CMD:    next_state = AVG_VEC;
-            MAN_CMD:    next_state = MAN_DIST;
-            EUC_CMD:    next_state = EUC_DIST;
+            default: begin
+              next_state = OP;
+              dist_accum = 20'd0;
+            end
           endcase
         end
       end
 
       WRITE_BRAM: begin
+        core_lock = 1'b1;
         enable_write = 1'b1;
         next_state = WRITE_BRAM;
+
+        if (~bram_sel) begin
+          brama_write_addr = write_address;
+          brama_write_en = write_enable;
+        end 
+        else begin
+          bramb_write_addr = write_address;
+          bramb_write_en = write_enable;
+        end
 
         if (write_done) next_state = IDLE;
       end
 
       READ_VEC: begin
+        core_lock = 1'b1;
+        next_state = READ_VEC;
+
+        if (~bram_sel) begin
+          brama_fetch = 1'b1;  
+
+          if (brama_byte_read) begin
+            tx_data = brama_out_data;
+            tx_start = 1'b1;       
+            next_state = TO_HOST;
+          end
+        end
+        else begin
+          bramb_fetch = 1'b1;
+
+          if (bramb_byte_read) begin
+            tx_data = bramb_out_data;
+            tx_start = 1'b1;       
+            next_state = TO_HOST;
+          end
+        end
+
+        if (brama_read_done | bramb_read_done) begin
+          next_state = IDLE;
+        end
+      end
+
+      FETCH: begin
+        core_lock = 1'b1;
+        next_state = FETCH;
+
+        brama_fetch = 1'b1;
+        bramb_fetch = 1'b1;
+
+        if (brama_byte_read & bramb_byte_read) next_state = OP;
+        if (brama_read_done & bramb_read_done) begin
+          if ((cmd_dec == MAN_CMD) | (cmd_dec == EUC_CMD)) next_state = OP;
+          else next_state = IDLE;
+        end
         
       end
 
-      SUM_VEC: begin
-        
-      end
+      OP: begin
+        core_lock = 1'b1;
+        next_state = OP;
 
-      AVG_VEC: begin
-        
-      end
+        case (cmd_dec)
 
-      MAN_DIST: begin
-        
-      end
+          SUM_CMD: begin
+            if (brama_read_done & bramb_read_done)
 
-      EUC_DIST: begin
-        
+            tx_data = brama_read + bramb_read;
+            tx_start = 1'b1;
+
+            next_state = TO_HOST;
+          end
+
+          AVG_CMD: begin
+            tx_data = brama_read >> 1 + bramb_read >> 1;
+            tx_start = 1'b1;
+            next_state = TO_HOST;
+          end
+
+          MAN_CMD: begin
+            if (brama_read_done & bramb_read_done) begin
+              tx_data = dist_accum >> ADDRESS_WIDTH;
+              tx_start = 1'b1;
+              next_state = TO_HOST;
+            end
+            else begin
+              diff = brama_read[7:0] - bramb_read[7:0];
+              man_accum_flag = 1'b1;
+              next_state = FETCH; // Maybe check man_accum_done before
+            end 
+          end
+
+          EUC_CMD:
+            if (brama_read_done & bramb_read_done) begin
+              dist_accum
+            end
+            else begin
+              diff = brama_read[7:0] - bramb_read[7:0];
+              euc_accum_flag = 1'b1; // Maybe check euc_accum_done before
+              next_state = FETCH;
+            end
+
+        endcase
       end
 
       TO_HOST: begin
-        
+        core_lock = 1'b1;
+        if (~tx_busy) begin
+          case (cmd_dec)
+            WRITE_CMD:  next_state = WRITE_BRAM;
+            READ_CMD:   next_state = READ_VEC;
+            SUM_CMD:    next_state = FETCH;
+            AVG_CMD:    next_state = FETCH;
+            MAN_CMD:    next_state = IDLE;
+            EUC_CMD:    next_state = IDLE;
+          endcase
+        end
       end
+
     endcase
   end
+
+  // FSM WRITE
+  write_control #(
+    .MEMORY_DEPTH(MEMORY_DEPTH),
+    .ADDRESS_WIDTH(ADDRESS_WIDTH)
+  )
+  FSM_WRITE
+  (
+    .clk(CLK100MHZ),
+    .reset(reset),
+    .enable(enable_write),
+    .rx_ready(rx_ready),
+    .write_enable(write_enable),
+    .done(write_done),
+    .address(write_address)
+  );
+
+  // FSM READ & TX FOR BRAMA
+  read_control #(
+    .MEMORY_DEPTH(MEMORY_DEPTH),
+    .ADDRESS_WIDTH(ADDRESS_WIDTH),
+    .WAIT_READ_CYCLES(WAIT_READ_CYCLES)
+  )
+  FSM_READ_BRAMA
+  (
+    .clk(CLK100MHZ),
+    .reset(reset),
+    .fetch(brama_fetch),
+    .read_address(brama_read_addr),
+    .done(brama_read_done)
+    .byte_read(brama_byte_read)
+  );
+
+  // FSM READ & TX FOR BRAMB
+  read_control #(
+    .MEMORY_DEPTH(MEMORY_DEPTH),
+    .ADDRESS_WIDTH(ADDRESS_WIDTH),
+    .WAIT_READ_CYCLES(WAIT_READ_CYCLES)
+  )
+  FSM_READ_BRAMB
+  (
+    .clk(CLK100MHZ),
+    .reset(reset),
+    .fetch(bramb_fetch),
+    .read_address(bramb_read_addr),
+    .done(bramb_read_done)
+    .byte_read(bramb_byte_read)
+  );
+
+  // CAST IN TOP TOP TOP MODULE
+  /*
+  // Memory block BRAMA
+  blk_mem_gen_0 BRAMA (
+    .clka(CLK100MHZ),    // input wire clka
+    .wea(brama_write_en),      // input wire [0 : 0] wea
+    .addra(brama_write_addr),  // input wire [9 : 0] addra
+    .dina(rx_data),    // input wire [7 : 0] dina
+    .clkb(CLK100MHZ),    // input wire clkb
+    .addrb(brama_read_addr),  // input wire [9 : 0] addrb
+    .doutb(brama_read)  // output wire [7 : 0] doutb
+  );
+
+  // Memory block BRAMB
+  blk_mem_gen_0 BRAMB (
+    .clka(CLK100MHZ),    // input wire clka
+    .wea(bramb_write_en),      // input wire [0 : 0] wea
+    .addra(bramb_write_addr),  // input wire [9 : 0] addra
+    .dina(rx_data),    // input wire [7 : 0] dina
+    .clkb(CLK100MHZ),    // input wire clkb
+    .addrb(bramb_read_addr),  // input wire [9 : 0] addrb
+    .doutb(bramb_read)  // output wire [7 : 0] doutb
+  );
+  */
+  
--- tarea_2/coprocessor/coprocessor.srcs/sources_1/new/read_control.sv
+++ tarea_2/coprocessor/coprocessor.srcs/sources_1/new/read_control.sv
@@ -24,24 +24,24 @@ module read_tx  #(
   parameter MEMORY_DEPTH = 8,
   parameter ADDRESS_WIDTH = 8,
   parameter WAIT_READ_CYCLES = 3
-  )(
-  input logic clk,
-  input logic reset,
-  input logic enable,
-  input logic tx_busy,
-  output logic [ADDRESS_WIDTH-1:0] read_address,
-  output logic done,
-  output logic tx_start
-    );
+)
+(
+  input   logic clk,
+  input   logic reset,
+  input   logic fetch,
+  output  logic [ADDRESS_WIDTH-1:0] read_address,
+  output  logic done,
+  output  logic byte_read
+);
 
   // FSM logic
-  typedef enum logic [2:0] {IDLE, READ, WAIT_READ, ADDRESS, DONE} state;
+  typedef enum logic [2:0] {IDLE, READ, WAIT_READ, PUSH, ADDRESS, DONE} state;
   state current_state, next_state;
 
   // Inner Logic
   logic max_address, waited, count_enable, over_address;
   localparam  WAIT_WIDTH = $clog2(WAIT_READ_CYCLES);
-  logic[WAIT_WIDTH-1:0] wait_cycles;
+  logic [WAIT_WIDTH - 1:0] wait_cycles;
 
   // FSM READ_TX
   always_ff @ (posedge clk) begin
@@ -52,11 +52,12 @@ module read_tx  #(
   always_comb begin
     next_state = IDLE;
     done = 1'b0;
-    tx_start = 1'b0;
     count_enable = 1'b0;
-    case (current_state)
+    byte_read = 1'b0;
 
-      IDLE: if(enable) next_state = READ;
+    case (current_state)
+    
+      IDLE: if (fetch) next_state = READ;
 
       READ: begin
         if (over_address) next_state = DONE;
@@ -64,18 +65,28 @@ module read_tx  #(
       end
 
       WAIT_READ: begin
-        if(waited) next_state = ADDRESS;
-        else next_state = WAIT_READ;
+        next_state = WAIT_READ;
+        if (waited) next_state = PUSH;
+      end
+
+      PUSH: begin
+        next_state = PUSH;
+        byte_read = 1'b1;
+        if (~fetch) begin
+          next_state = ADDRESS;
+        end
       end
 
       ADDRESS: begin
         count_enable = 1'b1;
-        next_state = READ;
+        next_state = IDLE;
       end
 
       DONE: begin
         done = 1'b1;
-        next_state = IDLE;
+        if (~fetch) begin
+          next_state = IDLE;
+        end
       end
 
     endcase
@@ -83,19 +94,19 @@ module read_tx  #(
 
   //address_counter
   address_counter #(
-      .MEMORY_DEPTH(MEMORY_DEPTH),
-      .ADDRESS_WIDTH(ADDRESS_WIDTH)
-      )
-      READ_ADDRESS
-      (
-      .clk(clk),
-      .reset(reset),
-      .enable(count_enable),
-      .clear(done),
-      .address(read_address),
-      .max_address(max_address),
-      .over_address(over_address)
-      );
+    .MEMORY_DEPTH(MEMORY_DEPTH),
+    .ADDRESS_WIDTH(ADDRESS_WIDTH)
+  )
+  READ_ADDRESS
+  (
+    .clk(clk),
+    .reset(reset),
+    .enable(count_enable),
+    .clear(done),
+    .address(read_address),
+    .max_address(max_address),
+    .over_address(over_address)
+  );
 
 
   // WAIT counter

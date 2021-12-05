abf87bbf7b8d370f3bc56365333e343d418582a1
pabloreyesrobles <pabloreyes500@gmail.com>
2021-12-05 19:39:26 -0300

    op_test abs test

--- tarea_2/coprocessor/coprocessor.srcs/sim_1/new/op_test.sv
+++ tarea_2/coprocessor/coprocessor.srcs/sim_1/new/op_test.sv
@@ -1,17 +1,18 @@
-`timescale 1us / 1us
+`timescale 1ns / 1ps
 
 module op_testbench ();
   logic [7:0] data_a, data_b;
   logic [15:0] data_c;
-  
-  // generate a clock signal that inverts its value every five time units
-	always  #1 clk=~clk;
 
   initial begin
     data_a = 8'd127;
     data_b = 8'd255;
     
     #10 data_c = data_a - data_b;
+    #10 if (data_c[15] == 1) data_c = -data_c;
+    #10 data_a = -8'd13;
+    #10 if (data_a[7] == 1) data_c = (-data_a[7:0]) * (-data_a[7:0]);
+        else data_c = data_a * data_a;
   end
 
 endmodule
